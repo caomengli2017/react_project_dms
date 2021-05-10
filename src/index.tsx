f356f2cc7fb666b551c9e9be734d8c741acc69b7
暮鼓晨钟 <>
2021-05-10 17:32:59 +0800

    add env

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux';
 
 import store from '@src/redux/store';
 import './index.less';
+import HttpApi from './utils/https';
 
 switch (process.env.REACT_APP_ENV) {
   case 'local':
@@ -13,11 +14,13 @@ switch (process.env.REACT_APP_ENV) {
     // HttpApi.baseURL = 'http://localhost:3000';
     break;
   case 'dev':
-    // HttpApi.baseURL = 'http://apitest.baiweietech.com';
+    HttpApi.baseURL = 'http://apitest.baiweietech.com';
     break;
   case 'test':
+    HttpApi.baseURL = 'http://apitest.baiweietech.com';
     break;
   default:
+    HttpApi.baseURL = 'http://apitest.baiweietech.com';
     break;
 }
 
--- src/page/main/shop/shop-list/index.tsx
+++ src/page/main/shop/shop-list/index.tsx
@@ -4,10 +4,6 @@
  *@Date: 2021-05-08 17:51:41
  */
 import { FBaseListPage } from '@src/component';
-import React from 'react';
-import { Button, Input } from 'antd';
-import intl from 'react-intl-universal';
-
 const ShopListPage = () => {
   return (
     <FBaseListPage

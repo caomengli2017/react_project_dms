cc53b58d1781c1f2b9efd35c24345ffaae2aad34
jackjia <jack.jia@heavengifts.com>
2021-05-12 11:42:41 +0800

    shoplist

--- src/apis/main/shop.ts
+++ src/apis/main/shop.ts
@@ -1,5 +1,14 @@
 import HttpApi from '@src/utils/https';
 
+// 获取店铺列表
+export function getShopList(params?: any) {
+  return HttpApi.request({
+    url: '/admin/stores/list',
+    method: 'POST',
+    data: params,
+  });
+}
+
 // 获取店铺申请列表
 export function getShopApplicationList(params?: any) {
   return HttpApi.request({
--- src/page/main/shop/shop-list/index.tsx
+++ src/page/main/shop/shop-list/index.tsx
@@ -3,14 +3,93 @@
  *@desc 店铺列表
  *@Date: 2021-05-08 17:51:41
  */
-import { FBaseListPage } from '@src/component';
+import React, { useState } from 'react';
+import { Button, Input, Select, Typography } from 'antd';
+import { getShopList } from '@src/apis/main/shop';
+import { FBaseListPage, FFormItemRangePicker } from '@src/component';
+import { PlusOutlined } from '@ant-design/icons';
 const ShopListPage = () => {
+  const [visible, setvisible] = useState(false);
+  const showModal = () => {
+    setvisible(true);
+  };
+  console.log(123);
   return (
     <FBaseListPage
-      queryApi=""
+      queryApi={getShopList}
       rowKey="id"
-      conditions={[]}
-      columns={[]}
+      leftNode={[
+        <Button
+          type="primary"
+          icon={<PlusOutlined />}
+          onClick={() => showModal()}
+        >
+          新建店铺
+        </Button>,
+      ]}
+      conditions={[
+        {
+          id: 'storeId',
+          label: '店铺id',
+          _node: <Input placeholder="请输入店铺id" />,
+        },
+        {
+          id: 'storeName',
+          label: '店铺名称',
+          _node: <Input placeholder="请输入店铺名称" />,
+        },
+        {
+          id: 'storeType',
+          label: '店铺类型',
+          _node: (
+            <Select placeholder="请选择店铺类型">
+              <Select.Option value={2}>经销商直营门店</Select.Option>
+            </Select>
+          ),
+        },
+        {
+          id: 'status',
+          label: '店铺状态',
+          _node: (
+            <Select placeholder="请选择店铺状态">
+              <Select.Option value={1}>营业中</Select.Option>
+              <Select.Option value={0}>关闭</Select.Option>
+            </Select>
+          ),
+        },
+        {
+          id: 'timeFilter',
+          label: '下单时间',
+          _node: <FFormItemRangePicker />,
+        },
+      ]}
+      columns={[
+        { dataIndex: 'storeId', title: '店铺id' },
+        { dataIndex: 'storeName', title: '店铺名称' },
+        { dataIndex: 'storeType', title: '店铺类型' },
+        { dataIndex: 'createdAt', title: '加入时间' },
+        {
+          dataIndex: 'status',
+          title: '状态',
+          render: (value) => {
+            return value === 1 ? '营业中' : '关闭';
+          },
+        },
+        {
+          title: '操作',
+          render: (value, record) => {
+            return (
+              <Typography.Link
+                onClick={() => {
+                  setvisible(true);
+                }}
+              >
+                编辑
+              </Typography.Link>
+            );
+          },
+        },
+      ]}
     ></FBaseListPage>
   );
 };
